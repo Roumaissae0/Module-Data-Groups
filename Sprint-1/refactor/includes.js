3ca73854ed212434cb85ce9b053fc44f5d1f8ed1
Roumaissae <alroumaissaebb@gmail.com>
2026-05-19 17:55:13 +0200

    implement includes function without built-in methods

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,11 +1,8 @@
 // Refactor the implementation of includes to use a for...of loop
 
-function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
-      return true;
-    }
+function includes(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) return true;
   }
   return false;
 }
