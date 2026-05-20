975edfd0152373e1f308d57faf315664216b2311
Roumaissae <alroumaissaebb@gmail.com>
2026-05-20 20:47:15 +0200

    implement tally function and add tests

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,15 @@
-function tally() {}
-
+function tally() {
+  if (!Array.isArray(items)) {
+    throw new Error('Input must be an array');
+  }
+  const result = {};
+  for (const item of items) {
+    if (result[item]) {
+      result[item]++;
+    } else {
+      result[item] = 1;
+    }
+  }
+  return result;
+}
 module.exports = tally;
--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -23,7 +23,25 @@ const tally = require("./tally.js");
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object", () => {
+  expect(tally([])).toEqual({});
+});
+
+test("tally on an array with one item returns count of 1", () => {
+  expect(tally(['a'])).toEqual({ a: 1 });
+});
+
+test("tally on an array with duplicate items returns correct counts", () => {
+  expect(tally(['a', 'a', 'a'])).toEqual({ a: 3 });
+});
+
+test("tally on an array with multiple different items returns correct counts", () => {
+  expect(tally(['a', 'a', 'b', 'c'])).toEqual({ a: 2, b: 1, c: 1 });
+});
+
+test("throws an error when passed a string", () => {
+  expect(() => tally('abc')).toThrow('Input must be an array');
+});
 
 // Given an array with duplicate items
 // When passed to tally
